cb6e1aa0cc1b620a769924f9c7cd1ce8b86624e4
azizul <azizul45551@gmail.com>
2023-05-12 02:39:13 -0400

    kdsal;k---------------|

--- src/com/Xy/Add.java
+++ src/com/Xy/Add.java
@@ -4,6 +4,8 @@ public class Add {
 
 	public static void main(String[] args) {
 	System.out.println("adding sone poiunt-----------------------------");
+	
+	System.out.println("jkhdsjfhljahlk;hlkjsdarhi ");
 
 	}
 
